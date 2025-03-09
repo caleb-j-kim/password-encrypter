114dfd07a18677c117c60f5b5bddb28dd308a7c4
caleb-j-kim <coolycaleb@gmail.com>
2025-03-08 23:01:02 -0600

    Created logger.cpp file

---
+++ logger.cpp
@@ -0,0 +1,57 @@
+/* Caleb Kim
+   3/8/2025
+   CS 4348.501
+   Child process that logs status of encrypting passwords.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+string currentTimeStamp() { // Function to get the current time
+    auto now = chrono::system_clock::now();
+    time_t now_time = chrono::system_clock::to_time_t(now);
+    tm local_tm;
+    localtime_r(&now_time, &local_tm);
+    stringstream ss;
+    ss << put_time(localtime(&now_c), "%F %T");
+    return ss.str();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <logfile>" << endl;
+        return 1;
+    }
+
+    ofstream logFile(argv[1], ios::app);
+    if(!logFile) {
+        cerr << "Error opening glog file." << endl;
+        return 1;
+    }
+
+    string line;
+    while(getline(cin, line)) {
+        if(line == "QUIT")
+            break;
+
+        istringstream iss(line);
+        string action;
+        iss >> action;
+        string message;
+        getline(iss, message);
+        if(!message.empty() && message[0] == ' ')
+            message = message.substr(0, 1);
+        logFile << currentTimeStamp() << " [" << action << "] " << message << endl;
+    }
+
+    logFile.close();
+    return 0;
+}
